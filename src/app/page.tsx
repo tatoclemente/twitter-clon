aeb6015a6f1a082a8c4ea1a412be4aaa1083c834
tatoclemente <tatoapuros@gmail.com>
2023-10-28 18:26:23 -0300

    Conexion with Supabase And Login With GitHub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import Image from 'next/image'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+import { AuthBotton } from './components/auth-botton'
 
-export default function Home() {
+
+export default async function Home () {
+  const cookieStore = cookies()
+
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+      },
+    }
+  )
+  const { data: posts } = await supabase.from('posts').select('*')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      ¡Hola twitter!
+      <AuthBotton />
+
+      <pre>{JSON.stringify(posts, null, 2)}</pre> 
     </main>
   )
 }
