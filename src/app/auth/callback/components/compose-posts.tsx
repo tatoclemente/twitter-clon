7f9f5528c3376888a0669a1a936ed92b3eac95c6
tatoclemente <tatoapuros@gmail.com>
2023-11-07 19:24:01 -0300

    Add validation in Post button

--- src/app/auth/callback/components/compose-posts.tsx
+++ src/app/auth/callback/components/compose-posts.tsx
@@ -1,17 +1,34 @@
 "use client";
 
 import { Avatar } from "@nextui-org/react";
-import { ComposeTextArea } from "./compose-textarea";
 import { addPost } from "./compose-add-post";
-import { useRef } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 export function ComposePost({ userAvatarUrl }: { userAvatarUrl: string }) {
   const formRef = useRef<HTMLFormElement>(null);
+  const [state, setState] = useState("");
+  const [disabled, setDisabled] = useState<boolean>(true);
+
+
+   useEffect(() => {
+    if (state.length > 0) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+   }, [state])
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const {value} = e.target;
+    setState(value);
+  }
 
   return (
     <form
       ref={formRef}
       action={async (formData) => {
+
+        // if (formRef.current?.nodeValue === '') return
         await addPost(formData);
         formRef.current?.reset();
       }}
@@ -29,12 +46,13 @@ export function ComposePost({ userAvatarUrl }: { userAvatarUrl: string }) {
           autoComplete="off"
           type="text"
           name="content"
+          onChange={handleChange}
           // rows={4}
           className="w-full sm:text-1xl lg:text-2xl ml-2 pl-2 bg-white placeholder-gray-500 focus:outline-none focus:border-b-1 focus:pb-6"
           placeholder="¡¿Qué está pasando?!"
         />
       </div>
-      <button className="bg-[#1D9CF0] text-white font-bold rounded-full w-24 px5 py-1.5 self-end">
+      <button disabled={disabled} className="bg-[#1D9CF0] text-white font-bold rounded-full w-24 px5 py-1.5 self-end disabled:opacity-50">
         Postear
       </button>
     </form>
